9e3771a1a16296e5d193029de3ee9cd60069f462
StuMcA <stuart.mcara@outlook.com>
2021-03-16 20:34:19 +0000

    initial commit

---
+++ console.py
@@ -0,0 +1,24 @@
+import pdb
+from models.task import Task
+import repositories.task_repository as task_repository
+from models.user import User
+import repositories.user_repository as user_repository
+
+user_repository.delete_all()
+task_repository.delete_all()
+
+user_1 = User("Jack", "Jarvis")
+user_2 = User("Victor", "McDade")
+task_1 = Task("Walk Dog", user_1, 60)
+task_2 = Task("Feed Cat", user_2, 5)
+
+user_repository.save(user_1)
+user_repository.save(user_2)
+
+task_repository.save(task_1)
+task_repository.save(task_2)
+
+task_1.mark_complete()
+task_repository.update(task_1)
+
+pdb.set_trace()
